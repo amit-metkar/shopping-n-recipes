96e2304782fb150f01963343a0204f8a0a814191
Metkar <amit.metkar@corp.trizetto.com>
2017-07-24 19:27:04 +0530

    dropdown directive

---
+++ src/app/shared/directives/dropdown.directive.ts
@@ -0,0 +1,35 @@
+import {
+  Directive,
+  TemplateRef,
+  ElementRef,
+  Renderer2,
+  ViewContainerRef,
+  HostListener,
+  HostBinding
+} from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+
+export class DropdownDirective {
+
+    @HostBinding('class.open') isOpen: boolean = false;
+
+    constructor(private elementRef: ElementRef, 
+        private renderer: Renderer2,
+        private vcRef: ViewContainerRef) {
+    }
+
+    @HostListener('click') toggleOpen(eventData: Event) {
+        this.isOpen = !this.isOpen;
+    }
+
+    // @HostListener('click') onclick(eventData: Event) {
+    //     this.renderer.addClass(this.elementRef.nativeElement, 'open');
+    // }
+
+    // @HostListener('blur') onblur(eventData: Event) {
+    //     this.renderer.removeClass(this.elementRef.nativeElement, 'open');
+    // }
+}
