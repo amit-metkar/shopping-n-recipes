a281140fde09694300c1b67bd21eea762071097e
Amit Metkar <amit.metkat@gmail.com>
2017-09-17 22:46:19 +0530

    fixed warnings

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,16 +1,16 @@
 import { Ingredient } from '../shared/ingredient.model';
 
 export class Recipe {
-  // public name: string;
-  // public description: string;
-  // public imagePath: string;
-  // public ingredients: Ingredient[];
+  public name: string;
+  public description: string;
+  public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(private name: string, private description: string, private imagePath: string, private ingredients: Ingredient[]) {
-    // this.name = name;
-    // this.description = description;
-    // this.imagePath = imagePath;
-    // this.ingredients = ingredients;
+  constructor(name: string, description: string, imagePath: string, ingredients: Ingredient[]) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
   getIngredients() {
     return this.ingredients;
